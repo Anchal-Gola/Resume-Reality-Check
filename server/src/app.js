47c03b1bbba6b08725fa1a1ca96f794490125dfc
Anchal-Gola <anshikagola45@gmail.com>
2026-08-12 23:13:53 +0530

    Fix Vercel deployment CORS

--- server/src/app.js
+++ server/src/app.js
@@ -8,8 +8,8 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "https://resume-reality-check-dusky.vercel.app",
+  "https://resume-reality-check-gk88f8ryl-anshikagola45-3597s-projects.vercel.app",
 ];
-
 app.use(
   cors({
     origin: allowedOrigins,
